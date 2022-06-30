82a9dfcff26ffed79e878bbf3ce781350e1aa535
Jan T. Olsen <janthomasolsen@gmail.com>
2022-06-30 11:52:52 +0200

    Moved and added dataclass specific functions to generic dataclass

--- toolbox.py
+++ toolbox.py
@@ -10,7 +10,7 @@
 #           [26.06.2022] - Jan T. Olsen
 
 # Import packages
-from dataclasses import dataclass, field, fields, is_dataclass
+from dataclasses import dataclass, field, fields, is_dataclass, replace
 import socket
 import struct
 
@@ -108,7 +108,7 @@ class RemoteConfig(_CommConfig):
         self.Config = (self.IP, self.Port)
 
 # Check if object is iterable
-def checkIterable(object) -> bool:
+def _iterable(object) -> bool:
     """
     Check if input-object is iterable
     (note String is not considered as iterable type)
@@ -130,8 +130,8 @@ def checkIterable(object) -> bool:
         # Return non-iterable status
         return False
 
-# Find Byte Conversion Code of input
-def findConversionCode(indata) -> str:
+# Get Byte Conversion Code of input-data
+def get_conversioncode(indata) -> str:
     """
     Find the correct Conversion-Code of input-data
     Using the _COMM_CONT dataclass
@@ -171,24 +171,24 @@ def findConversionCode(indata) -> str:
     return conversioncode
 
 # Pack Data to Bytes
-def packToBytes(indata) -> tuple[bytes, str]:
+def pack_to_bytes(indata) -> tuple[bytes, str]:
     """
     Pack data to byte
     Get data from input data and pack them to bytes 
     with correct conversion-code for the related data-types
     Packed-data can be used for data-transfer over TCP/UDP
     :param indata: Data to be packed to bytes
-    :return dataPacked: Packed data (bytes)
+    :return packed_data: Packed data (bytes)
     :return conversionCode: Conversion-Code of packed data (str)
     """
 
     # Initialize Function outputs
-    conversionCode = '' 
-    dataPacked = b''
+    conversioncode = '' 
+    packed_data = b''
     data = 0
 
     # Check if in-data is iterable
-    if checkIterable(indata):
+    if _iterable(indata):
         # Data is redefined as list
         data = []
 
@@ -198,7 +198,7 @@ def packToBytes(indata) -> tuple[bytes, str]:
             _data = element
 
             # Get and Update Conversion Code
-            conversionCode += findConversionCode(_data)
+            conversioncode += get_conversioncode(_data)
 
             # String: Special case
             if type(_data) is str:
@@ -209,12 +209,12 @@ def packToBytes(indata) -> tuple[bytes, str]:
             data.append(_data)
         
         # Pack data to bytes
-        dataPacked = struct.pack(_COMM_CONST.Network + conversionCode, *data)
+        packed_data = struct.pack(_COMM_CONST.Network + conversioncode, *data)
     
     # In-data is not iterable
     else:
         # Get and Update Conversion Code
-        conversionCode += findConversionCode(indata)
+        conversioncode += get_conversioncode(indata)
 
         # Special case: String
         if type(indata) is str:
@@ -227,70 +227,13 @@ def packToBytes(indata) -> tuple[bytes, str]:
             data = indata
 
         # Pack data to bytes
-        dataPacked = struct.pack(_COMM_CONST.Network + conversionCode, data)
+        packed_data = struct.pack(_COMM_CONST.Network + conversioncode, data)
 
     # Function return 
-    return dataPacked, conversionCode
-
-# Pack Dataclass to byte
-def packToBytes_DataClass(dataclass) -> tuple[ bytes, str, list]:
-    """
-    Pack DataClass to byte
-    Get data entries from dataclass and pack them to bytes 
-    with correct conversion-code for the related data-types
-    Packed-data can be used for data-transfer over TCP/UDP
-    :param dataclass: Dataclass to be packed to bytes
-    :return data: Dataclass data (list)
-    :return dataPacked: Packed Dataclass data (bytes)
-    :return conversionCode: Conversion-Code of packed data (str)
-    """
-
-    # Initialize Function outputs
-    conversionCode = '' 
-    dataPacked = b''
-    indata = 0
-
-    # Declare field variables
-    field_name = []
-    field_type = []
-    field_value = []  
-
-    # Ensure that incomming data is a dataclass
-    if is_dataclass(dataclass):
-        # Loop through the fields of the dataclass
-        for field in fields(dataclass):
-            # Get information for current field of data
-            _name = field.name
-            _value = dataclass.__getattribute__(_name)
-
-            # Get and Update Conversion Code
-            conversionCode += findConversionCode(_value)
-
-            # Special case: String
-            if type(_value) is str:
-                # String needs to be encoded to byte-value
-                _value = _value.encode('UTF-8')
-
-            # Append acquired information to arrays
-            field_name.append(_name)
-            field_value.append(_value)
-
-        # Dataclass Data
-        indata = field_value
-
-        # Pack Dataclass data to byte
-        dataPacked = struct.pack(_COMM_CONST.Network + conversionCode, *indata)
-    
-    # Incomming data is NOT dataclass
-    else:
-        # Report error
-        raise TypeError('ERROR: pack2byte_DataClass: Incomming data is NOT dataclass {%s}' %type(dataclass))
-
-    # Function return 
-    return dataPacked, conversionCode, indata
+    return packed_data, conversioncode
 
 # Unpack Data from Bytes
-def unpackFromBytes(conversionCode : str, packedData : bytes):
+def unpack_from_bytes(conversioncode : str, packed_data : bytes):
     """
     Unpack data from bytes
     The packed data is converted back to its original type(s)
@@ -301,26 +244,25 @@ def unpackFromBytes(conversionCode : str, packedData : bytes):
     :return data: Unpacked Data
     """
     # Define local variables
-    _unpackedData = b''
+    _unpacked_data = b''
     _value = ''
     _index = 0
 
     # Unpack data from bytes to a local variable
-    _unpackedData = struct.unpack(_COMM_CONST.Network + conversionCode, packedData)
+    _unpacked_data = struct.unpack(_COMM_CONST.Network + conversioncode, packed_data)
 
     # Determine if Packed-Data contains more than one-variable
-    if len(conversionCode) > 1:
+    if len(conversioncode) > 1:
 
         # Define Unpacked-Data as list
-        unpackedDataList = []
+        _unpacked_data_list = []
         
         # Iterate through Conversion-Code
-        for code in conversionCode:
-            
+        for code in conversioncode:
+    
             # Special case: Digit
             if code.isdigit():
-                # If a digit is present in the Conversion-Code
-                # its related to the length of a future string
+                # Digits are only related to the length of a future string
                 # and has no related value in the Packed-Data
 
                 # Skip this iteration
@@ -329,80 +271,201 @@ def unpackFromBytes(conversionCode : str, packedData : bytes):
             # Special case: String
             elif code == _COMM_CONST.STR:
                 # String needs to be decoded from byte-value
-                _value = _unpackedData[_index].decode('UTF-8')
+                _value = _unpacked_data[_index].decode('UTF-8')
 
             # Assign the local value-variable equal to the indexed element of unpackedData
             else:
-                _value = _unpackedData[_index]
+                _value = _unpacked_data[_index]
 
             # Update the index number
             _index += 1
 
             # Append index value to Unpacked-Data list
-            unpackedDataList.append(_value)
+            _unpacked_data_list.append(_value)
 
         # Check length of list
-        if len(unpackedDataList) > 1:
+        if len(_unpacked_data_list) > 1:
 
             # Unpacked-Data-List contains more than one entry
             # Convert Unpacked-Data-List to a tuple
-            unpackedData = tuple(unpackedDataList)
+            unpacked_data = tuple(_unpacked_data_list)
 
         # List length is only one entry
         else:
             # Assign the Unpacked-Data equals to the only entry of the list
-            unpackedData = unpackedDataList[-1]
+            unpacked_data = _unpacked_data_list[-1]
 
     # Only one variable to unpack
     else:
         # Assign the Unpacked-data equals to the previously found local variable
-        unpackedData = _unpackedData
+        unpacked_data = _unpacked_data
 
     # Function return
-    return unpackedData
-
-# Unpack Data from Bytes
-def unpackFromBytes_DataClass(conversionCode : str, packedData : bytes) -> tuple:
-    """
-    Unpack Data-class from bytes 
-    The packed data is converted back to its original dataclass type(s)
-    using the given conversion-code
-    Unpacked-data can be used for data-received over TCP/UDP
-    :param dataPacked: Packed data (bytes)
-    :param conversionCode: Conversion-Code of packed data (str)
-    :return data: Unpacked Data (tuple)
-    """
-    raise NotImplementedError()
-
+    return unpacked_data
 
 @dataclass
-class testData():
+class _CommDataclass():
+    """
+    Generic Data-Class for communication 
+    This Data-Class contains functions related to updating its attributes, 
+    finding the Data-Class' Conversion-Code, aswell as packing and unpacking
+    the Data-Class to and from bytes (respectively) 
+    """
+
+    # Update Dataclass Attributes
+    def _update_attributes(self, data_dict : dict):
+        """
+        Update Dataclass Attributes
+        Using data-dictionary (with keys and values) to update the Dataclass attributes
+        Usually used together with unpacking from bytes to dataclass 
+        :param data_dict: Data-Dictionary in relation to Dataclass attributes
+        """
+        # Iterate through Data-item
+        for key, value in data_dict.items():
+            # Check if Dataclass has the attribute 'key'
+            if hasattr(self, key):
+                # Set Dataclass attribute 'key' equal to value
+                setattr(self, key, value)
+
+    # Get Dataclass Conversion-Code
+    def get_conversioncode(self) -> str:
+        """
+        Get Dataclass Conversion-Code
+        The Conversion-Code is generated based on the Dataclass attributes, and their respective types.
+        This functions used the "Toolbox.get_conversioncode()" to obtain the correct Conversion-Code
+        of the input data, using the "Toolbox._COMM_CONST"
+        :return conversioncode: Dataclass Conversion-Code (str)
+        """
+        # Define Conversion-Code
+        conversioncode : str = ''
+
+        # Iterate through the fields of the dataclass
+        for field in fields(self):
+            # Get data for current field
+            _name = field.name
+            _value = self.__getattribute__(_name)
+
+            # Find and Update Conversion Code
+            conversioncode += get_conversioncode(_value)
+
+        # Return Conversion-Code of Dataclass
+        return conversioncode
+
+    # Pack Dataclass to Bytes
+    def pack_to_bytes(self) -> tuple[bytes, str]:
+        """
+        Pack the Dataclass to Bytes
+        Get data entries from dataclass and pack them to bytes 
+        with correct conversion-code for the related data-types
+        Packed-data can be used for data-transfer over TCP/UDP
+        :return packed_dataclass: Packed Dataclass data (bytes)
+        :return conversioncode: Dataclass Conversion-Code (str)
+        """
+        # Define data to be packed as list
+        data : list = []    
+
+        # Iterate through the fields of the dataclass
+        for field in fields(self):
+            # Get data for current field
+            _name = field.name
+            _value = self.__getattribute__(_name)
+
+            # Special case: String
+            if type(_value) is str:
+                # String needs to be encoded to byte-value
+                _value = _value.encode('UTF-8')
+            
+            # Append acquired information to arrays
+            data.append(_value)
+
+        # Get Dataclass Conversion-Code
+        conversioncode = self.get_conversioncode()
+
+        # Pack Dataclass data to byte
+        packed_dataclass = struct.pack(_COMM_CONST.Network + conversioncode, *data)
+
+        # Function return
+        return packed_dataclass, conversioncode
+
+    # Unpack Dataclass from Bytes
+    def unpack_from_bytes(self, packed_dataclass : bytes, conversioncode : str) -> tuple:
+        """
+        Unpack the Dataclass from Bytes
+        The packed data is converted back to its original Dataclass type(s) using the given conversion-code. 
+        This function used the "Toolbox.unpack_from_bytes()" to convert the bytes back to their original type(s) 
+        A check on the input conversion-code and the related conversion-code of the dataclass
+        is made to ensure incomming data matches the attributes of the dataclass
+        Unpacked-data can be used for data-transfer over TCP/UDP
+        :param packed_dataclass: Packed data (bytes)
+        :param conversionCode: Conversion-Code of packed dataclass (str)
+        :return unpacked_dataclass: Packed Dataclass data (bytes)
+        :return conversioncode: Dataclass Conversion-Code (str)
+        """
+        # Define local variables
+        _data_dict : dict = {}                  # Data dictionary to update the dataclass
+        _index : int = 0                        # Loop-Index
+        _conversioncode : str = ''              # Local variable for Input Conversion-Code
+        _dataclass_conversioncode : str  = ''   # Local variable for Dataclass Conversion-Code
+
+        # Remove digits from Conversion-Code
+        # ------------------------------
+        # (Digits are only related to the length of a future string
+        # and has no related value in the Packed-Data)
+        # Iterate through items of Dataclass Conversion-Code
+        for i in conversioncode:
+            # Check for non-digits
+            if not i.isdigit():
+                # Add only non-digit values to local variable for Input Conversion-Code
+                _conversioncode += _conversioncode.join(i)
+
+        # Iterate through items of Dataclass Conversion-Code
+        for j in self.get_conversioncode():
+            # Check for non-digits
+            if not j.isdigit():
+                # Add only non-digit values to local variable for Dataclass Conversion-Code
+                _dataclass_conversioncode += _dataclass_conversioncode.join(j)
+
+        # Compare and ensure that the incomming Conversion-Code matches with the Dataclass Conversion-Code
+        if _conversioncode == _dataclass_conversioncode:
+
+            # Unpack data from bytes
+            unpacked_dataclass = unpack_from_bytes(conversioncode, packed_dataclass)
+
+            # Iterate through the fields of the dataclass
+            for field in fields(self):
+                
+                # Update Data-dictionary
+                # with key from current field-name and value from unpacked data at index
+                _data_dict[field.name] = unpacked_dataclass[_index]
+
+                # Update loop-index
+                _index += 1
+
+            # Update DataClass with Unpacked data
+            self._update_attributes(_data_dict)
+            
+            # Return Unpacked data
+            return unpacked_dataclass
+
+        # Conversion-Codes does NOT match
+        else:
+            # Report error
+            raise ValueError('ERROR: unpack_from_bytes: Incomming Conversion-Code does NOT match dataclass {%s}' %type(dataclass))
+
+@dataclass
+class testData(_CommDataclass):
     name : str = 'jan'
     age : int = 29
     heigth : float = 1.85
 
-    # _typeConversion : str = ''
+@dataclass
+class testData2(_CommDataclass):
+    name : str = 'olsen'
+    # age : bool = False
+    age : int = 12
+    heigth : float = 1.70
 
-    # def __post_init__(self):
-    #     # Loop through the fields of the dataclass
-    #     for field in fields(self):
-    #         _name = field.name
-    #         _value = self.__getattribute__(_name)
-    #         _type = type(_value)
-
-    #         print(_name)
-    #         print(_value)
-    #         print(_type)
-    #         # _typeConversion
-    def update(self, name : str, age : int, height : float):
-        self.name = name
-        self.age = age
-        self.heigth = height
-
-    def updateWithTuple(self, data : tuple):
-        
-        self.update(data[0], data[1], data[2])
-
+    
 # Main Function
 # ------------------------------
 if __name__ == "__main__":
@@ -411,27 +474,56 @@ if __name__ == "__main__":
     # TEST & DEBUG
     # ------------------------------
 
-
     # data_test = ('hei jan', 'hei birger', 88.99, True)
     # print(data_test)
     # print('\n')
     
     data_test = testData()
     print(data_test)
+    print(data_test.get_conversioncode())
+    print('\n')
 
+    data_test2 = testData2()
+    print(data_test2)
+    print(data_test2.get_conversioncode())
+    print('\n')
 
-    # packedData, convCode = packToBytes(data_test)
-    packedData, convCode, datain = packToBytes_DataClass(data_test)
+    packedData, convCode = data_test.pack_to_bytes()
     print(packedData)
     print(convCode)
     print('\n')
 
-    unpackedData = unpackFromBytes(convCode, packedData)
-    print(unpackedData)
+    data_test2.unpack_from_bytes(packedData, convCode)
+    print(data_test2)
     print('\n')
 
-    data_test.updateWithTuple(unpackedData)
-    print(data_test)
+    # # kai = testData(name='kai',age=88, heigth=1.92)
+    # kai = testData2()
+    # print(kai)
+    # print('\n')
+
+    # unpackedData = unpack_from_bytes_dataclass(kai, convCode, packedData)
+    # print(unpackedData)
+    # print(kai)
+    # print('\n')
+
+    # kai.update_dict(unpackedData)
+    
+
+    # ------------------------------
+
+    # # packedData, convCode = pack_to_bytes_dataclass(data_test)
+    # packedData, convCode, datain = pack_to_bytes_dataclass(data_test)
+    # print(packedData)
+    # print(convCode)
+    # print('\n')
+
+    # unpackedData = unpack_from_bytes(convCode, packedData)
+    # print(unpackedData)
+    # print('\n')
+
+    # data_test.updateWithTuple(unpackedData)
+    # print(data_test)
 
 
     # # kai = testData(name='kai',age=88, heigth=1.92)
